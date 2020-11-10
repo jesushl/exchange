c1f16c51c7772008c3b60c9121e7aaf6ae4fa97a
jesushledon@gmail.com <jesushledon@gmail.com>
2020-11-09 20:18:10 -0600

    removing not necessary bootstrap dependency

--- exchange/exchange/settings.py
+++ exchange/exchange/settings.py
@@ -38,10 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # external
-    "bootstrap4",
+    "rest_framework",
     # aplications
     "harvesting",
-    "rest_framework",
 ]
 
 MIDDLEWARE = [
